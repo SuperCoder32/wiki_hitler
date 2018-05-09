b7085cc7c0fe79732a49d9479d65c8b0817c02ca
Peter Petrov <pietarcho@gmail.com>
2018-05-09 14:49:48 +0300

    Now program displays more information about progress

--- proof_final.py
+++ proof_final.py
@@ -3,44 +3,75 @@ from link_extractor_final import get_all_articles
 
 
 def get_neighbours(title):
-	return get_all_articles(title)
+    return get_all_articles(title)
 
 
-def main(root_article):
+parent_of = dict()
 
-	toVisit = [(root_article, 0)]
-	visited = set()
-	proven = True
-        steps = 0
+def backtrack(node):
+    degree = 0
+    
+    while node is not None:
+        print(node, end='')
+        node = parent_of[node]
 
-	while len(toVisit) > 0:
+        if node is not None:
+            print('->', end='')
+            degree += 1
 
-		label, depth = toVisit.pop()
-
-		print(steps, len(visited), label, depth)
-
-		neighbours = get_neighbours(label)
-
-		if depth > 5:
-			proven = False
-			break
-
-		for link in neighbours:
-			if link in visited:
-				continue
-
-			newNode = (link, depth + 1)
-			toVisit.insert(0, newNode)
-			visited.add(link)
-                
-                steps += 1
+    print('\n')
+    return degree
 
 
-	success = "{} is reachable in {} clicks from any wiki article".format(root_article, depth)
-	failure = "{} cannot be reached in 5 clicks or less from any wiki article".format(root_article)
+def main(root_article, verbose):
+    global parent_of
+    parent_of = dict()
 
-	print(success if proven else failure)
+    toVisit = [root_article]
+    parent_of[root_article] = None
+
+    visited = set()
+    proven = True
+    steps = 0
+
+    while len(toVisit) > 0 and proven:
+        node = toVisit.pop() 
+
+        if verbose:
+            print("Added neighbours of", steps, "articles", end='; ')
+            print("A total of", len(visited), "articles are visited", end='; ')
+            print("There are", len(toVisit), "articles to add their neighbours in queue")
+            
+        degree = backtrack(node)
+        neighbours = get_neighbours(node)
+
+        if degree == 6 - 1 and len(neighbours) > 0:
+            proven = False
+            break
+
+        for link in neighbours:
+            if link in visited:
+                continue
+
+            toVisit.insert(0, link)
+            parent_of[link] = node
+            visited.add(link)
+        
+        steps += 1
+
+
+    success = "{} is reachable in {} clicks from any wiki article".format(root_article, degree)
+    failure = "{} cannot be reached in 6 clicks or less from any wiki article".format(root_article)
+
+    print(success if proven else failure)
 
 
 if __name__ == "__main__":
-	main(sys.argv[1])
+        root_article = sys.argv[1]
+
+        try:
+            verbose = sys.argv[2] == '-v'
+        except IndexError:
+            verbose = False            
+
+        main(root_article, verbose)
